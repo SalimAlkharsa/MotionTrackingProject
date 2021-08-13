51ab032a58ec318bb99a660129a690cdc6fdee0c
Salim Al Kharsa <61260042+SalimAlkharsa@users.noreply.github.com>
2021-08-12 22:13:53 -0500

    Update OneLastProgramThatneedsWork.py

--- OneLastProgramThatneedsWork.py
+++ OneLastProgramThatneedsWork.py
@@ -8,12 +8,17 @@ import matplotlib.pyplot as plt
 #Loading and formatting the df
 df = pd.read_csv('CleanData.csv', error_bad_lines=False)
 df = df.drop(["Unnamed: 0"], axis=1)
-#Creating the new df
-RowsNeeded = 11*len(df.index)
+
+
+#Creating the new df in a good format
+Tracked = int(input(("How many objects were tracked? ")))
+RowsNeeded = Tracked*len(df.index)
 NewDF = pd.DataFrame(np.empty((RowsNeeded, 6)) * np.nan) 
 NewDF_Cols = ["Object","Category","Frame","x","y","Size"]
 NewDF.columns = NewDF_Cols
 
+
+#Now populating the names
 #Creating the objects column properly
     #First I need to get the names without the x and y
 Cols = df.columns.values.tolist()
@@ -21,13 +26,14 @@ ObjectName = []
 for i in Cols:
     if "_x" in i:
         ObjectName.append(str(i))
+      
         
     #Now I have the names and I need to make the list properly    
 CurrentRows = 0
 RowNames = []
 while CurrentRows != RowsNeeded:
     RowNames.append(ObjectName)
-    CurrentRows = len(RowNames*11)
+    CurrentRows = len(RowNames*Tracked)
 ObjectName = []
 for item in RowNames:
     for i in item:
@@ -35,32 +41,12 @@ for item in RowNames:
 RowNames = []
 NewDF["Object"] = pd.Series(ObjectName)
 
-    #Now I need to make the category
-CurrentRows = 0
-Cats = []
-Categories = ["Team 1", "Team 2", "Ball"]
-for i in range(11):
-    if i < 5:
-        Cats.append(Categories[0])
-    elif i >= 5 and i < 10:
-        Cats.append(Categories[1])
-    else:
-        Cats.append(Categories[2])
-Categories = []
-while CurrentRows != RowsNeeded:
-    Categories.append(Cats)
-    CurrentRows = len(Categories*11)
-Cats = []
-for item in Categories:
-    for i in item:
-        Cats.append(i)
-Categories = []
-NewDF["Category"] = pd.Series(Cats)
+
     #Now I will make frame
 CurrentRows = 0
 Frames = []
-for i in range(int(RowsNeeded/11)):
-    for x in range(11):
+for i in range(int(RowsNeeded/Tracked)):
+    for x in range(Tracked):
         Frames.append(i)
 NewDF["Frame"] = pd.Series(Frames)  
     #Now I will make size
@@ -100,17 +86,4 @@ YVALS = Adjuster(df, NewDF, indicator = "y")
 NewDF["x"] = pd.Series(XVALS)
 NewDF["y"] = pd.Series(YVALS)
 
-#Saving
 NewDF.to_csv("GraphedReady.csv")
-
-#Plotting
-
-Plot = px.scatter(range_x=[-10,1920], range_y=[-10,1080])
-
-Plot = px.scatter(
-    x=NewDF["x"],
-    y=NewDF["y"],
-    animation_frame=NewDF["Frame"],
-    range_x=[-10,1280], range_y=[-10,1280],color = NewDF["Category"], size = NewDF["Size"])
-
-plot(Plot)
